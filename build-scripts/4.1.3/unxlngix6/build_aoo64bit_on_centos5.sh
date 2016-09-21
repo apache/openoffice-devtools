6b142110dd021444bef46b73b90e8b4a675c47b0
Andrea Pescetti <pescetti@apache.org>
2016-09-21 21:27:14 +0000

    Use current URLs for dmake and epm.
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1761816 13f79535-47bb-0310-9956-ffa450edef68

--- build-scripts/4.1.3/unxlngix6/build_aoo64bit_on_centos5.sh
+++ build-scripts/4.1.3/unxlngix6/build_aoo64bit_on_centos5.sh
@@ -22,8 +22,8 @@ autoconf && ./configure   \
 	--enable-gstreamer \
 	--with-package-format="rpm deb" \
 	--with-lang="${LANGS}" \
-	--with-dmake-url=http://dmake.apache-extras.org.codespot.com/files/dmake-4.12.tar.bz2 \
-	--with-epm-url=http://www.msweet.org/files/project2/epm-3.7-source.tar.gz
+	--with-dmake-url=http://sourceforge.net/projects/oooextras.mirror/files/dmake-4.12.tar.bz2 \
+	--with-epm-url=http://sourceforge.net/projects/oooextras.mirror/files/epm-3.7.tar.gz
 
 source ./LinuxX86-64Env.Set.sh || exit 1 
 ./bootstrap || exit 1
