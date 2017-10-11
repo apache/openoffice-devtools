eedc2e255d6746ddb44f51cdc6718e9c4e49e815
Jim Jagielski <jim@apache.org>
2017-10-11 21:03:23 +0000

    Build updates for AOO-4.1.4-RC5 on Linux 32
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1811890 13f79535-47bb-0310-9956-ffa450edef68

--- build-scripts/4.1.4/unxlngi6/build_aoo32bit_on_centos5.sh
+++ build-scripts/4.1.4/unxlngi6/build_aoo32bit_on_centos5.sh
@@ -31,7 +31,7 @@ fi
 	--with-lang="${LANGS}" \
 	--with-dmake-url=http://sourceforge.net/projects/oooextras.mirror/files/dmake-4.12.tar.bz2 \
 	--with-epm-url=http://sourceforge.net/projects/oooextras.mirror/files/epm-3.7.tar.gz \
-	| tee config.out
+	| tee config.out || exit 1
 
 source ./LinuxX86Env.Set.sh || exit 1
 ./bootstrap || exit 1
