1f127ff6bb1bbf0f0d905e0b221743138f088800
Jim Jagielski <jim@apache.org>
2017-11-06 19:56:49 +0000

    Update w/ latest build
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1814434 13f79535-47bb-0310-9956-ffa450edef68

--- build-scripts/4.1.4/unxmacos/build_aooreleases.sh
+++ build-scripts/4.1.4/unxmacos/build_aooreleases.sh
@@ -125,7 +125,7 @@ fi
 	--without-stlport \
 	--with-package-format="installed dmg" \
 	--disable-systray \
-	--with-alloc=internal \
+	--with-alloc=system \
 	--with-lang="${LANGS}" \
 	| tee config.out || exit 1
 
