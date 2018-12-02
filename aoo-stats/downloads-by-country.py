eba3d2bf5889a69c4acc8802d4a6c1a3bc5b28eb
Matthias Seidel <mseidel@apache.org>
2018-12-02 15:20:54 +0000

    Fixed link, deleted whitespace
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1848003 13f79535-47bb-0310-9956-ffa450edef68

--- aoo-stats/downloads-by-country.py
+++ aoo-stats/downloads-by-country.py
@@ -1,5 +1,5 @@
 ################################################################
-# 
+#
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -7,16 +7,16 @@
 #  to you under the Apache License, Version 2.0 (the
 #  "License"); you may not use this file except in compliance
 #  with the License.  You may obtain a copy of the License at
-#  
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 #  Unless required by applicable law or agreed to in writing,
 #  software distributed under the License is distributed on an
 #  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 #  KIND, either express or implied.  See the License for the
 #  specific language governing permissions and limitations
 #  under the License.
-#  
+#
 ################################################################
 
 
@@ -39,7 +39,7 @@ def getSourceForgeStats(download, start_date, end_date):
     attempts = 0
 
     while attempts < 3:
-        try:    
+        try:
             conn = urllib.urlopen(url)
             data = conn.read()
             
@@ -51,7 +51,7 @@ def getSourceForgeStats(download, start_date, end_date):
 
     return ""
 
-# dictionary of language code to country_dict (dictionary of country name to  count)
+# dictionary of language code to country_dict (dictionary of country name to count)
 country_dict = {}
 
 def mergeCountries(countries):
@@ -68,7 +68,7 @@ def mergeCountries(countries):
 
 
 if len(sys.argv) == 0:
-    print "syntax:  python downloads-by-country.py <urls.lst> <start-date> <end-date>"
+    print "syntax: python downloads-by-country.py <urls.lst> <start-date> <end-date>"
     print "where <file.list> is a list of files URL's to gather stats on, and <start-date> and <end-date> are in YYYY-MM-DD format."
 
 downloads = [line.strip() for line in open(sys.argv[1])]
@@ -91,7 +91,7 @@ print "<head>"
 print "<title>OpenOffice Downloads by Country/Territory</title>"
 print "<html>"
 print "<body>"
-print "<p>This table shows downloads per <a href='http://en.wikipedia.org/wiki/Country_code_top-level_domain'>country/territory</a> for the period from " + sys.argv[2] + " to " + sys.argv[3] + ".</p>"
+print "<p>This table shows downloads per <a href='https://en.wikipedia.org/wiki/Country_code_top-level_domain'>country/territory</a> for the period from " + sys.argv[2] + " to " + sys.argv[3] + ".</p>"
 print "<table border='1' cellpadding='10'>"
 
 print "<tr>"
