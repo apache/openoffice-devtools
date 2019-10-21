8423481fd7a785c53fe80cac459c62ec02826e77
Jim Jagielski <jim@apache.org>
2019-10-21 13:05:52 +0000

    Sync macOS build scripts
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1868705 13f79535-47bb-0310-9956-ffa450edef68

--- build-scripts/4.1.8-dev/unxmacos/build_aoo64bit_on_macos.sh
+++ build-scripts/4.1.8-dev/unxmacos/build_aoo64bit_on_macos.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 #
-# Build-script for AOO 4.1.x on OSX 10.14
+# Build-script for AOO 4.2.x on OSX 10.13
 #
-# System Setup:
-#  XCode 7.3.1
-#  $ sudo xcode-select -s /Applications/Xcode7.app
+# OS:
+#   o OSX 10.14.6 (Mojave)
+#   o Xcode 11.1 w/ SDK 10.11
+#   o jdk1.7.0_80.jdk
+#   o jdk1.8.0_181.jdk
+#   o XCode 7.3.1
+#     $ sudo xcode-select -s /Applications/Xcode7.app
 #
 # Local Changes:
 #   MacPorts:
-#     o apache-ant (1.9.9)
-#     o gnutar (1.29) (symlink gnutar to gtar)
-#     o perl5 (perl5.22)
+#     o gnutar (1.29) (symlinked gnutar to gtar) (symlinked to ~/bin)
+#     o perl5 (perl5.22) (symlinked to ~/bin)
 #     o p5-archive-*
 #     o p5-lwp-*
 #     o p5-xml-parser
@@ -18,23 +21,23 @@
 #     o getopt (symlinked to ~/bin)
 #     o subversion
 #     o git
-#
-#   Oracle JAVA 1.7 JDK
-#
+##
 #   /usr/local:
 #   o Apache ant 1.9.13 / 1.10.4
 #   o dmake 4.12.3 (https://github.com/jimjag/dmake/archive/dmake-4.12.3.tar.gz)
 #   o epm 4.4.2 (https://github.com/jimjag/epm/archive/epm-4.4.2.tar.gz)
-#   o openssl (1.0.2p)
-#         https://www.openssl.org/source/openssl-1.0.2p.tar.gz
+#   o openssl (1.0.2t)
+#         https://www.openssl.org/source/openssl-1.0.2t.tar.gz
 #         $ export MACOSX_DEPLOYMENT_TARGET=10.7
 #         $ ./Configure darwin64-x86_64-cc no-shared --prefix=/usr/local --openssldir=/usr/local ; make install
-#   o libxml2-2.9.8 (--prefix=/usr/local --enable-shared=no --without-iconv)
-#   o libxslt-1.1.32 (--prefix=/usr/local --enable-shared=no)
+#   o libxml2-2.9.9 (--prefix=/usr/local --enable-shared=no --without-iconv)
+#   o libxslt-1.1.33 (--prefix=/usr/local --enable-shared=no)
 #   o pkg-config 0.29.2 (--prefix=/usr/local)
 #   o GNU patch 2.7.6 (--prefix=/usr/local)
 #
 #   Env:
+#     LC_CTYPE=en_US.UTF-8
+#     LANG=en_US.UTF-8
 #     LIBRARY_PATH=/usr/local/lib
 #     C_INCLUDE_PATH=/usr/local/include
 #     CPLUS_INCLUDE_PATH=/usr/local/include:/Applications/Xcode7.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include/c++/4.2.1
@@ -46,7 +49,7 @@
 #       linkings are to the static libs
 #
 #     o JDK 1.7 seems to work better; and it's always best to
-#       have just *one* version of te JDK installed.
+#       have just *one* version of the JDK installed.
 #
 
 #
@@ -66,8 +69,9 @@ AOO_BUILD_TYPE=
 AOO_BUILD_VERSION=
 AOO_BUILD_BETA=
 AOO_BUILD_DEV=
+AOO_BUILD_ALL="yes"
 
-AOPTS=`getopt -o vsjdba:j:m: --long verbose,skip-config,just-config,dev,beta,ant-version:,java-version:,macos-target: -n 'parse-options' -- "$@"`
+AOPTS=`getopt -o vsjdbqa:j:m: --long verbose,skip-config,just-config,dev,beta,quick,ant-version:,java-version:,macos-target: -n 'parse-options' -- "$@"`
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 #echo "$AOPTS"
 eval set -- "$AOPTS"
@@ -77,6 +81,7 @@ while true; do
     -v | --verbose ) AOO_VERBOSE_BUILD="--enable-verbose"; shift ;;
     -s | --skip-config ) AOO_SKIP_CONFIG="yes"; shift ;;
     -j | --just-config ) AOO_JUST_CONFIG="yes"; shift ;;
+    -q | --quick ) AOO_BUILD_ALL="no"; shift ;;
     -a | --ant-version ) AOO_ANT_VERSION=$2; shift 2 ;;
     -j | --java-version ) AOO_JAVA_VERSION=$2; shift 2 ;;
     -m | --macos-target ) AOO_MACOS_TARGET=$2; shift 2 ;;
@@ -86,7 +91,6 @@ while true; do
     * ) echo "unknown option: $1"; shift ;;
   esac
 done
-
 
 if [ ! -d ../main -o ! -d sal ] ; then
 	echo "CHDIR into AOO's main/ directory first!"
@@ -170,6 +174,7 @@ if [ "$AOO_SKIP_CONFIG" != "yes" ]; then
 	--with-package-format="dmg" \
 	--with-gxx-include-path="/Applications/Xcode7.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include/c++/4.2.1" \
 	--disable-systray \
+	--with-macosx-target=${AOO_MACOS_TARGET} \
 	--with-alloc=internal \
 	--with-lang="${LANGS}" \
 	| tee config.out ) || exit 1
@@ -192,7 +197,7 @@ elif [ "$AOO_BUILD_DEV" = "yes" ]; then
     dmake -P5 openofficedev  || exit 1
 	dmake -P5 sdkoodev_en-US || exit 1
 	dmake -P5 ooodevlanguagepack || exit 1
-else
+elif [ "$AOO_BUILD_ALL" = "yes" ]; then
 	dmake -P5 ooolanguagepack || exit 1
 	dmake -P5 sdkoo_en-US || exit 1 
 fi
