5ab44b734e9c5c8b5adcfe8193e5d88a5e445585
Jim Jagielski <jim@apache.org>
2021-01-09 15:11:15 +0000

    AOO419-RC1
    git-svn-id: https://svn.apache.org/repos/asf/openoffice/devtools@1885299 13f79535-47bb-0310-9956-ffa450edef68

--- build-scripts/4.1.9/unxlngix6/build_aoo64bit_on_centos5.sh
+++ build-scripts/4.1.9/unxlngix6/build_aoo64bit_on_centos5.sh
@@ -34,7 +34,6 @@ fi
 wget -O external/unowinreg/unowinreg.dll http://www.openoffice.org/tools/unowinreg_prebuild/680/unowinreg.dll
 
 LANGS="ast bg ca ca-XR ca-XV cs da de el en-GB en-US es eu fi fr gd gl he hi hu it ja km ko lt nb nl pl pt pt-BR ru sk sl sr sv ta th tr vi zh-CN zh-TW"
-LANGS="en-US de es fr ru it"
 
 if [ ! -e configure -o configure.in -nt configure ] ; then
 	echo "Running autoconf..."
